d1489ead4dc9cf96d7a672fa593165851e824f4a
Prakash <j6921@hotmail.com>
2019-09-24 21:02:24 -0700

    upload index.html and clean up some starter templates

--- crud_app.py
+++ crud_app.py
@@ -17,38 +17,30 @@ COLLECTION_NAME = 'olympic'
 
 @app.route("/")
 def index():
-    # olympic_info = mongo.db.olympic.find_one()
-    # return render_template("index.html",olympic_info=olympic_info)
-    # return jsonify(olympic_info)
-    # return jsonify({"key1":"val1", "key2":"val2"})
-    #return render_template("index.html",olympic_info=jsonify({"key1":"val1", "key2":"val2"}))
     return render_template("index.html")
 
 
-@app.route("/olympic_crud")
+@app.route("/uploadolympicdata")
 def olympic_upload_mongo():
     olympic = mongo.db.olympic
     olympic_dict = crud_file.olympic_crud()
 
     olympic.update({}, olympic_dict, upsert=True)
     return redirect("/", code=302)
-    #return ("/localhost/olympic_crud")
 
 @app.route("/downloadolympicdata")
-def olympic_download_mongo():
-    # olympic_info = mongo.db.olympic.find_one()
+def olympic_download_mongo():    
     connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
     collection = connection[DBS_NAME][COLLECTION_NAME]
     olympic_info = collection.find()
-    json_olympic_data = []
-    # for i in olympic_info["all_data"]:
+    json_olympic_data = []    
     for i in olympic_info:
         json_olympic_data.append(i)
     json_olympic_data = json.dumps(json_olympic_data, default=json_util.default)
     connection.close()
     return json_olympic_data
-    # return jsonify(olympic_info)
-
 
 if __name__ == "__main__":
+    # app.jinja_env.auto_reload = True
+    # app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.run(debug=True)
---
+++ static/js/logic.js
@@ -0,0 +1,34 @@
+console.log('logic.js')
+function buildPlot() {
+    /* data route */
+  var url = "/downloadolympicdata";
+  d3.json(url).then(function(response) {
+    console.log(response);
+    // console.log(response['all_data']);
+    // var data = response;
+
+    // var layout = {
+    //   scope: "usa",
+    //   title: "Pet Pals",
+    //   showlegend: false,
+    //   height: 600,
+    //         // width: 980,
+    //   geo: {
+    //     scope: "usa",
+    //     projection: {
+    //       type: "albers usa"
+    //     },
+    //     showland: true,
+    //     landcolor: "rgb(217, 217, 217)",
+    //     subunitwidth: 1,
+    //     countrywidth: 1,
+    //     subunitcolor: "rgb(255,255,255)",
+    //     countrycolor: "rgb(255,255,255)"
+    //   }
+    //};
+
+    // Plotly.newPlot("plot", data, layout);
+  });
+}
+
+buildPlot();
