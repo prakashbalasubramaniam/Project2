5f0f357df5ba828d0ad277d53298158de163581f
Prakash <j6921@hotmail.com>
2019-09-27 21:01:33 -0700

    upload logic.js for line chart

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,76 +2,74 @@ console.log('logic.js')
 
 function buildPlot() {
     
-    // olympic spinner code
-    $( document ).ready(function() {
+  // olympic spinner code
+  $( document ).ready(function() {
 	
-      // Loop through each nav item
-      $('.spinner-container').children('.spinner').each(function(index){
+    // Loop through each nav item
+    $('.spinner-container').children('.spinner').each(function(index){
           
-          // Turn the index value into a reasonable animation delay
-          var delay = index*0.3;
+      // Turn the index value into a reasonable animation delay
+      var delay = index*0.3;
           
-          // Apply the animation delay
-          // $(this).css("animation-delay", delay + "s")	
-    
+      // Apply the animation delay
+      // $(this).css("animation-delay", delay + "s")    
       });
-    });
+  });
   
-  //define SVG size
-var svgWidth = 960;
-var svgHeight = 500;
+  var svgWidth = 960;
+  var svgHeight = 500;
+  
+  var margin = {
+    top: 20,
+    right: 40,
+    bottom: 60,
+    left: 50
+  };
+  
+  var width = svgWidth - margin.left - margin.right;
+  var height = svgHeight - margin.top - margin.bottom;
 
-//define margins for SVG
-var margin = {
-  top: 20,
-  right: 40,
-  bottom: 80,
-  left: 100
-};
-
-//define drawing area
-var width = svgWidth - margin.left - margin.right;
-var height = svgHeight - margin.top - margin.bottom;
-
-//Create an SVG wrapper, append an SVG group that will hold our chart,
-//and shift the latter by left and top margins.
-var svg = d3.select("#line")
+  var svg = d3
+  .select(".line")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
 
-//Append an SVG group
-var chartGroup = svg.append("g")
+  var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-//Initial Params load
-var chosenXAxis = "Years";
-var chosenYAxis = "No. of Medals";  
-
-//var i = 0;
-var age = [];
   // download data from mongodb
   let line_url = "/downloadlinechartdata";
   d3.json(line_url).then(function(response) {
+    
     // output data to console
-    console.log(response[0].summer_female_medal); 
-      //parse data
-  
-    // response.forEach(function(data) {
-    //   //console.log(data);
-    //   //i++;
-    //   data.all_data.year = +data.all_data.year;      
-    //   console.log(+data.all_data.year);
-    //   console.log(i);
-    //   //i++;
-    // for (i = 0; i < response[0].all_data.length; i++) {
-    //   response[0].all_data[i].year = +response[0].all_data[i].year; 
-      //console.log(response[0].all_data[i].year);
-      //console.log(i);
-    //}
+    console.log(response[0].summer_female_medal.length); 
+    console.log(response[0]); 
+    
+    //function to convert string array to integer array
+    function ArraytoInt (inputArray) {
+      var outputArray = [];
+      for (var i = 0; i < inputArray.length; i++) {
+        outputArray[i] = parseInt(inputArray[i]);
+      }
+      console.log(outputArray);
+      return outputArray;
+    }
 
-    //console.log(response[0].all_data)
-  });
+    //convert all arrays from string into integer arrays
+    summer_female_medal_int = ArraytoInt(response[0].summer_female_medal);
+    summer_female_year_int = ArraytoInt(response[0].summer_female_year);
+    summer_male_medal_int = ArraytoInt(response[0].summer_male_medal);
+    summer_male_year_int = ArraytoInt(response[0].summer_male_year);
+    winter_female_medal_int = ArraytoInt(response[0].winter_female_medal);
+    winter_female_year_int = ArraytoInt(response[0].winter_female_year);
+    winter_male_medal_int = ArraytoInt(response[0].winter_male_medal);
+    winter_male_year_int = ArraytoInt(response[0].winter_male_year);
+
+
+
+
+    });
   
 
   //});
